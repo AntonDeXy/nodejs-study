8ffebcaa9c2aebc5c63f4711eaa5594b12510e33
AntonDeXy <porolyananton@gmail.com>
2019-09-18 14:24:53 +0300

    mongodb was connected

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectId
 
 const app = express()
+var db;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,24 +29,37 @@ app.get('/', function (req, res) {
 })
 
 app.get('/artists', function (req, res) {
-  res.send(artists)
+  db.collection('artists').find().toArray(function (err, docs){
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500)
+    } 
+    res.send(docs)
+  })
 })
 
 app.get('/artists/:id', function (req, res) {
-  console.log(req.params)
-  var artist = artists.find(function (artist) {
-    return artist.id === Number(req.params.id)
+  db.collection('artists').findOne({_id: ObjectID(req.params.id)}, function(err, doc){
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500)
+    } 
+    res.send(doc)
   })
-  res.send(artist)
 })
 
 app.post('/artists', function (req, res) {
-  let artist = {
-    id: Date.now(),
-    name: req.body.name,
+  var artist = {
+    name: req.body.name
   }
-  artists.push(artist)
-  res.send(artist)
+  db.collection('artists').insert(artist, function(err, result){
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500)
+    }
+    res.send(artist)
+  })
+  // res.send(artist)
 })
 
 app.put('/artists/:id', function(req, res) {
@@ -62,6 +78,13 @@ app.delete('/artists/:id', function (req, res) {
   res.sendStatus(200)
 })
 
-app.listen(3012, function () {
-  console.log("API started")
+MongoClient.connect('mongodb://localhost:27017/myapi',{ useUnifiedTopology:true, useNewUrlParser: true }, function(err, database){
+  if (err) {
+    return console.log(err)
+  }
+  db = database.db('myapi')
+
+  app.listen(3012, function () {
+    console.log("API started")
+  })
 })
