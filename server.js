6b18a29f63b23769cc4946f99cdcd9c60f685643
DeXy <podolyananton@gmail.com>
2019-09-18 12:50:48 +0300

    put && delete

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express') 
+const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-const artists = [
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var artists = [
   {
     id: 1,
     name: 'Metallica'
@@ -15,25 +19,49 @@ const artists = [
     id: 3,
     name: 'Deep Purple'
   }
-]
+];
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('Hello API')
 })
 
-app.get('/artists', function(req, res){
+app.get('/artists', function (req, res) {
   res.send(artists)
 })
 
-app.get('/artists/:id', function(req, res){
+app.get('/artists/:id', function (req, res) {
   console.log(req.params)
-  var artist = artists.find(function(artist){
+  var artist = artists.find(function (artist) {
     return artist.id === Number(req.params.id)
   })
   res.send(artist)
 })
 
+app.post('/artists', function (req, res) {
+  let artist = {
+    id: Date.now(),
+    name: req.body.name,
+  }
+  artists.push(artist)
+  res.send(artist)
+})
 
-app.listen(3012, function(){
+app.put('/artists/:id', function(req, res) {
+  var artist = artists.find(function (artist) {
+    return artist.id === Number(req.params.id)
+  })
+  artist.name = req.body.name
+  res.sendStatus(200)
+})
+
+
+app.delete('/artists/:id', function (req, res) {
+  artists = artists.filter(function (artist) {
+    return artist.id !== Number(req.params.id)
+  })
+  res.sendStatus(200)
+})
+
+app.listen(3012, function () {
   console.log("API started")
 })
